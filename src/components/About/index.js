bc34c34b7f91c0408a9b8124c75c1f9e84833a6e
ftan0025 <ftan0025@student.monash.edu>
2022-11-11 13:47:11 +1100

    create responsiveness about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -33,34 +33,34 @@ const About = () => {
             idx={15}
           />
         </h1>
-        <p>
-          I'm a penultimate year Monash University international student from
-          Indonesia, taking Bachelor of Computer Science with a minor in Mobile
-          App Development and Software Engineer. I'm keen and passionate in
-          coding since high school and all of my programming journey including
-          university life has taught me a lot of knowledge and skills relating
-          to it.
-        </p>
-        <p>
-          I always believe that the best way to learn is from experience.
-          Therefore, I always try to find projects, online courses, internships,
-          and opportunities relating to my career interest which is Web
-          Development or Software Development to fill in my free time.
-          Therefore, I am eager to find experience that can gradually improve my
-          soft and hard skill as an individual for continuous improvement.
-        </p>
-        <p>
-          I’m the type of person who really likes sceneries and I always feel 
-          like going out every time the
-          weather is great. Also, I really like to play any kind of sports,
-          especially Basketball. I feel like sports helps me not only to improve
-          my physical health, but mentally as well. I really like to talk to new
-          people in general and I considered myself as an easy going person
-          since I’m always interested in understanding people’s perspective. And
-          during free time, I usually fill in my time with the things that I’m
-          currently interested and passionate with, which is reading
-          books, exploring music, playing video games, and programming.
-        </p>
+        <div className="paragraphs">
+          <p>
+            I'm a penultimate year Monash University international student from
+            Indonesia, taking Bachelor of Computer Science with a minor in Mobile
+            App Development and Software Engineer. I'm keen and passionate in
+            coding since high school and all of my programming journey including
+            university life has taught me a lot of knowledge and skills relating
+            to it.
+          </p>
+          <p>
+            I always believe that the best way to learn is from experience.
+            Therefore, I always try to find projects, online courses, internships,
+            and opportunities relating to my career interest which is Web
+            Development or Software Development to fill in my free time.
+            Therefore, I am eager to find experience that can gradually improve my
+            soft and hard skill as an individual for continuous improvement.
+          </p>
+          <p>
+            I’m the type of person who really likes sceneries and I always feel
+            like going out every time the
+            weather is great. Also, I really like to play any kind of sports,
+            especially Basketball. I feel like sports helps me not only to improve
+            my physical health, but mentally as well.
+            During free time, I usually fill in my time with the things that I’m
+            currently interested and passionate with, which is reading
+            books, exploring music, playing video games, and programming.
+          </p>
+        </div>
       </div>
       {/* <div className="fontawesome-icon-background">
         <FontAwesomeIconBackground />
